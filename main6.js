6de5aba8320b61b7dbe3f073041219ab2ae191c0
RevuRavi <revathyrrr@gmail.com>
2024-02-01 12:16:16 +0300

    abc

--- main6.js
+++ main6.js
@@ -100,7 +100,7 @@ function add(a,b){
   var b;
   let result=a+b;
   console.log(result)
-}                         */
+}                         
 
 
 
@@ -113,3 +113,85 @@ console.log(addition(3,5));
 
 const vehicles = ['Swift', 'Blackhawk', 'Fortuner']; 
   
+
+////Method1:
+
+var obj = {key1: "value1", key2: "value2"};
+Object.assign(obj, {key3: "value3"});
+var val1,val2
+
+addobj()
+deleteObj()
+
+function addobj(){
+Object.assign(obj, {key4: "value4"});
+val1 = JSON.stringify(obj);
+val2=JSON.parse(val1);
+console.log(val2)
+}
+
+function deleteObj(){
+delete val2.key3
+console.log(val2)
+}
+*/
+
+//Method2:
+
+const fruits = new Map([
+  ["apples", 500],
+  ["bananas", 300],
+  ["oranges", 200]
+]);
+console.log(fruits)
+addobj();
+deleteobj()
+changePropertykey()
+
+function addobj(){
+fruits.set("mangoes", 100);
+console.log(fruits.size)
+console.log(fruits)
+}
+
+function deleteobj(){
+fruits.delete("apples")
+console.log(fruits.size)
+console.log(fruits)
+}
+ function changePropertykey(){
+     const keyss = fruits.keys();
+     console.log(keyss)
+     const key1=keyss.next().value;
+     console.log(key1)
+//changing property have to do
+ }
+
+
+
+
+//Method 3
+
+let obj={}
+obj={name:"parrot",color:"green"}
+console.log(obj)
+
+addObj("age","2months")
+addObj("size","small")
+
+deleteObj()
+
+var x;
+var y;
+var z;
+var w;
+function addObj(x,y){
+obj[x]=y;
+console.log(obj)
+}
+function deleteObj(){
+ delete obj.size;
+ console.log(obj)
+    
+}
+
